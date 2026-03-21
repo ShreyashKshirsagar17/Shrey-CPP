025d4b221e183dea0775cdbc26f3f96ab7f898eb
Shreysh Kshirsagar <shreyashk1730@gmail.com>
2026-03-21 11:22:33 +0530

    Add files via upload

---
+++ CPP20.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=4;
+    for(int i=1;i<=n;i++)
+    {
+        int num=1;
+        for(int j=1; j<=n;j++)
+        {
+            cout<<num;
+            num=num+1;
+        }
+        cout<<endl;
+    }
+}
